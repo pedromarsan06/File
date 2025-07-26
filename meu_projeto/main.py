0d0607e1e69cc38ef73da95390495267a8cb349b
Pedro Marques <pedromarsan06@gmail.com>
2025-07-26 11:48:48 -0300

    Primeiro commit do projeto FastAPI com SQLite

--- meu_projeto/main.py
+++ meu_projeto/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
 
+class user(BaseModel):
+	nome: str
+	idade: int
 @app.get('/')
 def homepage():
 	dic = {
 "lang":"python-termux", "page":"homepage", "lib":"FastAPI"
 }
 	return dic
+@app.post('/user/')
+def creat(User:user):
+	return {"mensagem":f"user = {User.nome}, years = {User.idade}"}
 
